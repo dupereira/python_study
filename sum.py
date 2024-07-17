f1bc56b40310c8aba006b7101b92f84e34b4e2ed
Eduarda Pereira da Silva <dupesillva@gmail.com>
2024-07-16 22:35:38 -0300

    variables and sum

---
+++ sum.py
@@ -0,0 +1,5 @@
+numero1 = input("Digite o primeiro número: ") 
+numero2 = input("Digite o segundo número: ")
+
+soma = int(numero1) + int(numero2)
+print(soma)
---
+++ variables.py
@@ -0,0 +1,9 @@
+number = 10
+flutuante = 2.5
+string = "duda"
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(type(number))
+print(type(flutuante))
+print(type(string))
+print(type(lista))
