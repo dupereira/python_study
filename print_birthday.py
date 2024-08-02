5772f781474e0b1703c49da6c3c7b2fc66ffd5f5
Eduarda Pereira da Silva <dupesillva@gmail.com>
2024-08-01 22:24:19 -0300

    print birthday and print name

---
+++ print_birthday.py
@@ -0,0 +1,17 @@
+def get_birthday():
+    dia = input('Dia: ')
+    mês = input('Mês: ')
+    ano = input('Ano: ')
+    resposta = input(f'Você nasceu no dia {dia} de {mês} de {ano}. Correto?(SIM/NAO): ')
+
+    if resposta.lower() == "sim":
+        print("Obrigada por confirmar. Em breve você será redirecionado.")
+    elif resposta.lower() == "nao":
+        print("Desculpa, pode me dizer novamente?")
+        get_birthday()
+    else:
+        print("Resposta incorreta. Reiniciando o programa.")
+        get_birthday()
+
+
+get_birthday()
